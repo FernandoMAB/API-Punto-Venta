8528d6f8305d696e08279b130176a1c8327957a1
Fernando A Mejía B <62525270+FernandoMAB@users.noreply.github.com>
2022-09-25 23:57:29 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -39,12 +39,8 @@ builder.Services.AddScoped<IDocumentoService,DocumentoService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
